3ad547ae5bc14763ed53fc9df672c238174863f8
Paras Gupta <37538941+CodexParas@users.noreply.github.com>
2024-10-19 18:51:35 +0530

    Initial commit

---
+++ src/main/java/com/paras/FreeAPIs/FreeApIsApplication.java
@@ -0,0 +1,13 @@
+package com.paras.FreeAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeApIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paras/FreeAPIs/FreeApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paras.FreeAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

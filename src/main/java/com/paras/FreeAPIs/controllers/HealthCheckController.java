cc464030591a068ce7250b9ad6dd31c98bdccddf
Paras Gupta <37538941+CodexParas@users.noreply.github.com>
2024-10-20 18:01:20 +0530

    Adds naming for Health Check APIs

--- src/main/java/com/paras/FreeAPIs/controllers/HealthCheckController.java
+++ src/main/java/com/paras/FreeAPIs/controllers/HealthCheckController.java
@@ -3,6 +3,7 @@ package com.paras.FreeAPIs.controllers;
 
 import com.paras.FreeAPIs.DTO.ResponseDTO;
 import com.paras.FreeAPIs.services.HealthCheckService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Health Check", description = "Health Check API")
 public class HealthCheckController {
 
     private final HealthCheckService healthCheckService;

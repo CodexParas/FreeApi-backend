ecc349b434adb7bb9117204b73cce2454ccf68a7
Paras Gupta <37538941+CodexParas@users.noreply.github.com>
2024-10-20 16:55:27 +0530

    Adds exception Handling

--- src/main/java/com/paras/FreeAPIs/handler/GlobalExceptionHandler.java
+++ src/main/java/com/paras/FreeAPIs/handler/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.paras.FreeAPIs.handler;
 import com.paras.FreeAPIs.DTO.ResponseDTO;
 import com.paras.FreeAPIs.exceptions.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
@@ -23,5 +27,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(BAD_REQUEST)
                 .body(ResponseDTO.error("Internal server error", e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleException (MethodArgumentNotValidException exp) {
+        Set<String> errors = new HashSet<>();
+        exp.getBindingResult().getAllErrors()
+           .forEach(error -> errors.add(error.getDefaultMessage())
+                   );
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(ResponseDTO.error("Validation error", errors.toString()));
     }
 }
